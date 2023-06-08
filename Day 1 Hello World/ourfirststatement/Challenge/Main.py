97075dc3ae789f56c846ba878454a2d028ed2215
egogarty <54384706+egogarty@users.noreply.github.com>
2023-06-08 20:42:58 +0100

    Create Main.py

---
+++ Day 1 Hello World/ourfirststatement/Challenge/Main.py
@@ -0,0 +1,8 @@
+print("Emer")
+print("February 22th 2023")
+print("""I am signing up for Replit's 100 days of Python challenges!
+I will make sure to spend some time every day coding along, for a minimum of 10 mintues a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone whenever I happen to be. No excuses for not coding from the middle of a field!
+""")
+print("I am feeling ☺️")
+print("You can follow my progress at replit.com/@emer2020")
