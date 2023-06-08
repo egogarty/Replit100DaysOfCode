c55a909433540e97f66a1715532101ef9521a382
egogarty <54384706+egogarty@users.noreply.github.com>
2023-06-08 20:41:42 +0100

    Create main.py

---
+++ Day 1 Hello World/ourfirststatement/main.py
@@ -0,0 +1,13 @@
+# single print
+print("Hello Replit")
+## Multiple Print Statements
+print("Well we")
+print("just use more lines")
+print("of code")
+### triple quote
+print("""Anything that starts
+with three quotes, and ends
+in three quotes can span
+many lines and even contain " symbols
+within it without freaking anything out!""")
+
